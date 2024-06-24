8c4677baf68464ba7263dba56321ed050e71ca96
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-06-24 08:24:04 +0300

    try catch

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -6,30 +6,33 @@ import s from "./MovieReviews.module.css";
 const MovieReviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState(null);
+
   useEffect(() => {
-    try {
-      const getReviews = async () => {
+    const getReviews = async () => {
+      try {
         const data = await fetchReviews(movieId);
         setReviews(data);
-      };
-      getReviews();
-    } catch (error) {
-      console.log(error);
-    }
+      } catch (error) {
+        console.log(error);
+    
+      }
+    };
+
+    getReviews();
   }, [movieId]);
+
   if (!reviews) return <h3>Loading...</h3>;
+
   return (
     <div className={s.wrapper}>
       {reviews && reviews.results && reviews.results.length > 0 ? (
         <ul className={s.list}>
-          {reviews.results.slice(0, 4).map((review) => {
-            return (
-              <li className={s.item} key={review.id}>
-                <h3 className={s.name}>{review.author}</h3>
-                <p className={s.text}>{review.content}</p>
-              </li>
-            );
-          })}
+          {reviews.results.slice(0, 4).map((review) => (
+            <li className={s.item} key={review.id}>
+              <h3 className={s.name}>{review.author}</h3>
+              <p className={s.text}>{review.content}</p>
+            </li>
+          ))}
         </ul>
       ) : (
         <h4 className={s.notFound}>
